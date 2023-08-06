b6e8018d705910bdfae82cb6489d1cb65b86a1cb
iftekharzeeon <1805038@ugrad.cse.buet.ac.bd>
2023-08-07 00:53:48 +0600

    Log updated

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use('/', router);
 
+app.get('/', (req, res) => {
+    res.send('Account service is up and running');
+});
+
 app.listen(port, () => {
     console.log(`Account service listening on port ${port}`);
 });
